6a44d823e6fe41539df5b91d1c7436ca35083e72
Junior Saul Maiz Echevarria <junior.maiz@unas.edu.pe>
2022-10-04 15:46:22 -0500

    reestructuracion del menubar de cliente natural- cliente juridico

--- resources/views/admin/operaciones/clientes/crearPersonaNatural.blade.php
+++ resources/views/admin/operaciones/clientes/crearPersonaNatural.blade.php
@@ -23,12 +23,14 @@
                         </div>
                         <ul class="nav nav-tabs">
                             <li class="nav-item">
-                              <a class="nav-link" aria-current="page" href="/admin/operaciones/clientes/crear">Cliente Natural</a>
+                                <a class="nav-link" aria-current="page" href="/admin/operaciones/clientes/crear">Cliente
+                                    Juridico</a>
                             </li>
                             <li class="nav-item">
-                              <a class="nav-link active" href="/admin/operaciones/clientes/crearPersonaNatural">Cliente Juridico</a>
+                                <a class="nav-link active" href="/admin/operaciones/clientes/crearPersonaNatural">Cliente
+                                    Natural</a>
                             </li>
-                          </ul>
+                        </ul>
                         <main>
                             <!-- Main page content-->
                             <div class="container-xl px-4 mt-4">
@@ -41,21 +43,24 @@
                                             <div class="card-body">
                                                 <label for="dni">Ingrese DNI</label>
                                                 <div class="input-group">
-                                                    <input type="number" min="1" max="99999999" id="dni" class="form-control" placeholder="Documento" />&nbsp;
-                                                    <button class="btn btn-primary" id="buscardni" align="right" onClick="consultarDni()">Buscar</button>
+                                                    <input type="number" min="1" max="99999999" id="dni"
+                                                        class="form-control" placeholder="Documento" />&nbsp;
+                                                    <button class="btn btn-primary" id="buscardni" align="right"
+                                                        onClick="consultarDni()">Buscar</button>
                                                 </div>
-                                                <form action="{{route('usuario.store')}}" method="post" enctype="multipart/form-data">
+                                                <form action="{{ route('usuario.store') }}" method="post"
+                                                    enctype="multipart/form-data">
                                                     @csrf
                                                     <!--search-->
                                                     <div class="d-grid gap-2 d-md-flex justify-content-md-end py-2 px-4">
 
                                                         <!--<form class="input-group">
-                                                                <input name="busqueda" class="form-control me-md-2"
-                                                                    type="search" placeholder="Ingrese DNI" aria-label="Search"
-                                                                    autocomplete="off">
-                                                                <button class="btn btn-outline-primary btn-sm" type="submit">
-                                                                    <em class='bx bx-search-alt'></em>Buscar</button>
-                                                            </form>-->
+                                                                    <input name="busqueda" class="form-control me-md-2"
+                                                                        type="search" placeholder="Ingrese DNI" aria-label="Search"
+                                                                        autocomplete="off">
+                                                                    <button class="btn btn-outline-primary btn-sm" type="submit">
+                                                                        <em class='bx bx-search-alt'></em>Buscar</button>
+                                                                </form>-->
                                                     </div>
                                                     <!-- Form Group (username)-->
                                                     <div class="row gx-3 mb-3">
@@ -69,7 +74,7 @@
                                                         <div class="col-md-6">
                                                             <label class="small mb-1" for="inputName">Nombres</label>
                                                             <input class="form-control" name="nombre" id="nombres"
-                                                                type="text" placeholder="Nombres" readonly/>
+                                                                type="text" placeholder="Nombres" readonly />
                                                         </div>
                                                     </div>
                                                     <!-- Form Row-->
@@ -79,7 +84,7 @@
                                                             <label class="small mb-1" for="inputFirstName">Apellido
                                                                 Paterno</label>
                                                             <input class="form-control" name="apellido_paterno"
-                                                            id="apellido_paterno" type="text"
+                                                                id="apellido_paterno" type="text"
                                                                 placeholder="Apellido Paterno" readonly />
                                                         </div>
                                                         <!-- Form Group (last name)-->
@@ -87,22 +92,21 @@
                                                             <label class="small mb-1" for="inputLastName">Apellido
                                                                 Materno</label>
                                                             <input class="form-control" name="apellido_materno"
-                                                            id="apellido_materno" type="text"
+                                                                id="apellido_materno" type="text"
                                                                 placeholder="Apellido Materno" readonly />
                                                         </div>
                                                     </div>
                                                     <div class="row gx-3 mb-3">
                                                         <div class="col-md-6">
-                                                            <label class="small mb-1" for="inputEmail">correo electronico</label>
-                                                            <input class="form-control" name="correo"
-                                                                id="inputEmail" type="text"
-                                                                placeholder="example@rayosac.com" />
+                                                            <label class="small mb-1" for="inputEmail">correo
+                                                                electronico</label>
+                                                            <input class="form-control" name="correo" id="inputEmail"
+                                                                type="text" placeholder="example@rayosac.com" />
                                                         </div>
                                                         <div class="col-md-6">
                                                             <label class="small mb-1" for="inputTelephone">telefono</label>
-                                                            <input class="form-control" name="telefono"
-                                                                id="inputTelephone" type="text"
-                                                                placeholder="9208033951" />
+                                                            <input class="form-control" name="telefono" id="inputTelephone"
+                                                                type="text" placeholder="9208033951" />
                                                         </div>
                                                     </div>
                                                     <!-- Form Row        -->
@@ -129,7 +133,8 @@
                                                     <div class="row gx-2 mb-2">
                                                         <!-- select Estate-->
                                                         <div class="col-md-4">
-                                                            <label class="small mb-1" for="inputDepartamento">Departamento</label>
+                                                            <label class="small mb-1"
+                                                                for="inputDepartamento">Departamento</label>
                                                             <select class="form-select"
                                                                 aria-label="Default select example">
                                                                 <option selected>Seleccionar</option>
@@ -138,7 +143,8 @@
                                                         </div>
                                                         <!-- select roles and permissions-->
                                                         <div class="col-md-4">
-                                                            <label class="small mb-1" for="inputProvincia">Provincia</label>
+                                                            <label class="small mb-1"
+                                                                for="inputProvincia">Provincia</label>
                                                             <select class="form-select"
                                                                 aria-label="Default select example">
                                                                 <option selected>Seleccionar</option>
